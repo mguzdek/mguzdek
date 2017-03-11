a5785a5b3ab936d1372e1000572a1a81eb39dee0
mguzdek <guzdekmateusz@gmail.com>
2017-03-11 16:59:57 +0100

    Settings split

--- src/mguzdek/wsgi.py
+++ src/mguzdek/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mguzdek.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mguzdek.settings.production")
 
 application = get_wsgi_application()
